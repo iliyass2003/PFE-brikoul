84259b73e75d5d841c51edf6c2965b27f696bb5e
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2023-04-14 22:58:42 +0000

    detail page with its style

---
+++ src/pages/Detail.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase";
+import "../style/Detail.css";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [project, setProject] = useState(null);
+
+  useEffect(() => {
+    id && getProjectDetail();
+  }, [id]);
+
+  const getProjectDetail = async () => {
+    const docRef = doc(db, "projects", id);
+    const projectDetail = await getDoc(docRef);
+    setProject(projectDetail.data());
+  };
+  return (
+    <div className="project-detail">
+      <div className="title">{project?.title}</div>
+      <div className="infos">
+        <div className="category">{project?.category}</div>
+        <div>
+          <span className="author">{project?.author}</span> -{" "}
+          {project?.timestamp.toDate().toDateString()}
+        </div>
+      </div>
+      <img src={project?.imgUrl} alt={project?.title} />
+      <div className="description">{project?.description}</div>
+    </div>
+  );
+};
+
+export default Detail;
